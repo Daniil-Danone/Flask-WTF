fd531eacc946d1994ad57e3f6b617494aeb0efdf
Daniil <95695037+Daniil-Danone@users.noreply.github.com>
2022-03-12 03:05:27 +0600

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from requests import get, post
-
-
-print(get('http://127.0.0.1:8080/api/jobs').json())
-print(get('http://127.0.0.1:8080/api/jobs/1').json())
-print(post('http://127.0.0.1:8080/api/news',
-           json={'title': 'Заголовок'}).json())
